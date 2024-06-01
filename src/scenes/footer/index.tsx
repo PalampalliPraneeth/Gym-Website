c12adbc87ada086b9d7ecdf3f6fed0a2f1e2d38e
praneeth reddy <praneeth.reddy@tudip.com>
2024-06-01 15:35:35 -0400

    contact us change

--- src/scenes/footer/index.tsx
+++ src/scenes/footer/index.tsx
@@ -7,22 +7,20 @@ const Footer = () => {
         <div className="mt-16 basis-1/2 md:mt-0">
           <img alt="logo" src={Logo} />
           <p className="my-5">
-            Lorem vitae ut augue auctor faucibus eget eget ut libero. Elementum
-            purus et arcu massa dictum condimentum. Augue scelerisque iaculis
-            orci ut habitant laoreet. Iaculis tristique.
+          Your destination for fitness excellence. Experience state-of-the-art facilities, expert trainers, and diverse classes tailored to your fitness goals. Join us and unleash your full potential today
           </p>
           <p>© Evogym All Rights Reserved.</p>
         </div>
-        <div className="mt-16 basis-1/4 md:mt-0">
+        {/* <div className="mt-16 basis-1/4 md:mt-0">
           <h4 className="font-bold">Links</h4>
           <p className="my-5">Massa orci senectus</p>
           <p className="my-5">Et gravida id et etiam</p>
           <p>Ullamcorper vivamus</p>
-        </div>
+        </div> */}
         <div className="mt-16 basis-1/4 md:mt-0">
           <h4 className="font-bold">Contact Us</h4>
-          <p className="my-5">Tempus metus mattis risus volutpat egestas.</p>
-          <p>(333)425-6825</p>
+          <p className="my-5">Connect with us today and take the first step towards your fitness journey.</p>
+          <p>+91 9999999999</p>
         </div>
       </div>
     </footer>
