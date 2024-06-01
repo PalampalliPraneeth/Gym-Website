30216de62aa4a7b3b24268f4b211daa6059255af
praneeth reddy <praneeth.reddy@tudip.com>
2024-06-01 15:15:46 -0400

    changes to name

--- src/scenes/footer/index.tsx
+++ src/scenes/footer/index.tsx
@@ -11,7 +11,7 @@ const Footer = () => {
             purus et arcu massa dictum condimentum. Augue scelerisque iaculis
             orci ut habitant laoreet. Iaculis tristique.
           </p>
-          <p>© Flexgym All Rights Reserved.</p>
+          <p>© Evogym All Rights Reserved.</p>
         </div>
         <div className="mt-16 basis-1/4 md:mt-0">
           <h4 className="font-bold">Links</h4>
